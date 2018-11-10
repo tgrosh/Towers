36f848fc8a9374e8dbb5a7ff972285422874adb1
tgrosh <tgrosh@yahoo.com>
2018-11-09 21:17:22 -0500

    Missile Launcher

---
+++ Assets/Scripts/Missile.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : MonoBehaviour {
+    public float speed;
+    public GameObject explosionEffect;
+    public int damageAmount;
+    public float damageRadius;
+    public DamageType damageType;
+
+    Vector3 targetPosition;
+    Damage damageTarget;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (targetPosition != null)
+        {
+            transform.LookAt(targetPosition);
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+            if (Vector3.Distance(transform.position, targetPosition) < 1f)
+            {
+                DealDamage();
+                Explode();
+            }
+        }
+    }
+
+    public void Fire(Transform target)
+    {
+        targetPosition = target.position;
+    }
+
+    private void DealDamage()
+    {
+        Collider[] colliders = Physics.OverlapSphere(targetPosition, damageRadius);
+        foreach (Collider hit in colliders)
+        {
+            Damage damage = hit.GetComponent<Damage>();
+
+            if (damage != null)
+            {
+                damage.Harm(damageAmount, damageType);
+            }
+        }
+    }
+
+    private void Explode()
+    {
+        GameObject obj = Instantiate(explosionEffect, transform.position, transform.rotation, transform.parent);
+        Destroy(obj, 5);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/MissileLauncher.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissileLauncher : MonoBehaviour {
+    public GameObject currentTarget;
+    public GameObject body;
+    public float lookSpeed;
+    public float fireRate; //seconds per shot
+    public Missile projectilePrefab;
+    public Transform projectileSpawnPosition;
+    
+    List<GameObject> targets = new List<GameObject>();
+    float fireTimer;
+    float lookTime;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (targets.Count > 0)
+        {
+            if (targets[0] == null)
+            {
+                targets.RemoveAt(0);
+                lookTime = 0f;
+                return;
+            }
+
+            lookTime += Time.deltaTime;
+            Vector3 direction = targets[0].transform.position - body.transform.position;
+            Quaternion toRotation = Quaternion.LookRotation(direction);
+            body.transform.rotation = Quaternion.Slerp(body.transform.rotation, toRotation, lookSpeed * lookTime);
+
+            Fire();
+        }
+    }
+
+    private void Fire()
+    {
+        fireTimer += Time.deltaTime;
+
+        if (fireTimer > fireRate)
+        {
+            Missile projectile = Instantiate(projectilePrefab, projectileSpawnPosition.position, projectileSpawnPosition.rotation);
+            projectile.Fire(targets[0].transform);
+            fireTimer = 0f;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("EnemyAgent"))
+        {
+            targets.Add(other.gameObject);
+            lookTime = 0f;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("EnemyAgent"))
+        {
+            targets.Remove(other.gameObject);
+            lookTime = 0f;
+        }
+    }
+}
